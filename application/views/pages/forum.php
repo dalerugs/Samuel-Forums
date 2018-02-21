ca4e5faf207c43548454c055674aab03c265dee5
Patrick Dale Rugayan <pdale.rugayan@gmail.com>
2018-02-21 22:18:03 +0800

    update displaying of results

--- application/views/pages/forum.php
+++ application/views/pages/forum.php
@@ -43,8 +43,14 @@
 					<div class="col-md-8">
 						<h3>Conclusion</h3>
 						<p id="Conclusion">
-							
+						
 						</p>
+					</div>
+					<div class='loading col-md-12'>	`
+						<img class='center-block' src="<?php echo base_url("assets/Ellipsis.gif"); ?>" >
+						<h3 style="margin-top:-50px;text-align:center">
+							PLEASE WAIT . . . &nbsp; <span class='loading-status'></span>
+						</h3>
 					</div>
 				</div>
 				<div class="row margin-top">
@@ -71,6 +77,50 @@
 				</div>
 				<div class="row">
 					<div id="dashboard" class="col-md-12">
+						<div class='loading col-md-12'>	`
+							<img class='center-block' src="<?php echo base_url("assets/Ellipsis.gif"); ?>" >
+							<h3 style="margin-top:-50px;text-align:center">
+								PLEASE WAIT . . . &nbsp; <span class='loading-status'></span>
+							</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="row margin-top-20">
+	<div class="col-md-12">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>TEXT CLASSIFICATION</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div id="classification" class="col-md-12">
+						<table class="table table-bordered">
+								<thead>
+									<tr>
+										<td>POSITIVE</td>
+										<td>NEUTRAL</td>
+										<td>NEGATIVE</td>
+									</tr>
+									
+								</thead>
+								<tbody id="classification-table-body">
+
+								</tbody>
+							</table>
+							<div class='loading col-md-12'>	`
+								<img class='center-block' src="<?php echo base_url("assets/Ellipsis.gif"); ?>" >
+								<h3 style="margin-top:-50px;text-align:center">
+									PLEASE WAIT . . . &nbsp; <span class='loading-status'></span>
+								</h3>
+							</div>
+							
 					</div>
 				</div>
 			</div>
@@ -143,31 +193,94 @@
 		'text':corpus,
 		'summary_length':8,
 		'visualize': true,
-		'verbose': true,
-		'translate': true
 	};
 	$.ajax({
-		url: "http://192.168.1.14:63342/samuel_api?KEY=59FxFlXuh8nMuCPJ9qihmVVmyyroGvkpLPxdLne0",
+		url: "http://192.168.1.14:63342/samuel_api?KEY=wYXhoTGLXQCvARxkD60AA6rm01O6mh0u68nBnRTV",
 		type: 'POST',
 		data: JSON.stringify(data),
 		contentType:"application/json",
 		success:function(samuel) {
 		console.log(samuel);
+		clearInterval(interval);
+		$(".loading").hide();
+		// CONCLUSION
 		$("#Conclusion").text(samuel.summarized_text);
+
+		//  DASHBOARD
 		$("#dashboard").html(samuel.dashboard);
-		if (samuel.polarity=="positive") {
-			$("#Polarity").html("<i class='fa fa-smile-o' aria-hidden='true'></i> &nbsp;"+samuel.percentage.positive+" Positive")
+
+		// POLARITY & PERCENTAGE 
+		var varpositive = samuel.total_score.percentage.positive;
+		var varnegative = samuel.total_score.percentage.negative;
+		var varneutral = samuel.total_score.percentage.neutral;
+
+		var totalPosNeg = varpositive + varnegative;
+		var finalPos = ((varpositive/totalPosNeg)*100).toFixed(2);
+		var finalNeg = ((varnegative/totalPosNeg)*100).toFixed(2);
+
+		if(finalPos>finalNeg){
+			$("#Polarity").html("<i class='fa fa-smile-o' aria-hidden='true'></i> &nbsp;"+finalPos+" % Positive")
 			$("#Polarity").addClass("text text-success")
 		}
-		else if(samuel.polarity=="negative"){
-			$("#Polarity").html("<i class='fa fa-frown-o' aria-hidden='true'></i> &nbsp;"+samuel.percentage.negative+" Negative")
+		else{
+			$("#Polarity").html("<i class='fa fa-frown-o' aria-hidden='true'></i> &nbsp;"+finalNeg+" % Negative")
 			$("#Polarity").addClass("text text-danger")
 		}
-		else{
-			$("#Polarity").html("<i class='fa fa-meh-o' aria-hidden='true'></i> &nbsp;"+samuel.percentage.neutral+" Neutral")
-			$("#Polarity").addClass("text text-primary")
+
+		// TEXT CLASSIFICATION
+
+		var classificationTable = "";
+		for(var i = 0 ; i<samuel.score.length ; i++){
+			posDescriptors ="";
+			samuel.descriptors[i].pos.forEach(function(word) {
+				posDescriptors += word +", ";
+			});
+			neuDescriptors ="";
+			samuel.descriptors[i].neu.forEach(function(word) {
+				neuDescriptors += word +", ";
+			});
+			negDescriptors ="";
+			samuel.descriptors[i].neg.forEach(function(word) {
+				negDescriptors += word +", ";
+			});
+			
+			classificationTable += ""
+			+"<tr>"
+			+"<td><b>"+samuel.score[i].pos.toFixed(2)+"</b><br>"+posDescriptors+"</td>"
+			+"<td><b>"+samuel.score[i].neu.toFixed(2)+"</b><br>"+neuDescriptors+"</td>"
+			+"<td><b>"+samuel.score[i].neg.toFixed(2)+"</b><br>"+negDescriptors+"</td>"
+			+"</tr>";
 		}
+		$("#classification-table-body").html(classificationTable);
+
+		// if (samuel.polarity=="positive") {
+		// 	$("#Polarity").html("<i class='fa fa-smile-o' aria-hidden='true'></i> &nbsp;"+samuel.percentage.positive+" Positive")
+		// 	$("#Polarity").addClass("text text-success")
+		// }
+		// else if(samuel.polarity=="negative"){
+		// 	$("#Polarity").html("<i class='fa fa-frown-o' aria-hidden='true'></i> &nbsp;"+samuel.percentage.negative+" Negative")
+		// 	$("#Polarity").addClass("text text-danger")
+		// }
+		// else{
+		// 	$("#Polarity").html("<i class='fa fa-meh-o' aria-hidden='true'></i> &nbsp;"+samuel.percentage.neutral+" Neutral")
+		// 	$("#Polarity").addClass("text text-primary")
+		// }
+		},
+		error: function(xmlhttprequest, textstatus, message) {
+			clearInterval(interval);
+			$(".loading").html("<h1>Something went wrong. Please try again.</h1>");
 		}
 	});
+
+	var interval = setInterval(getProgress,1000);
+	function getProgress(){
+		$.ajax({
+			url: "http://192.168.1.14:63343/",
+			success: function(progress){
+				console.log(progress);
+				$(".loading-status").text(progress+" %")
+			}
+		});
+	}
 	
 </script>
